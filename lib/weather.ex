4e161b386902f12afc84b1c8e4126dff1de8008d
iggy <iggy.uga@gmail.com>
2018-01-07 19:26:37 -0500

    temperature option

--- lib/weather.ex
+++ lib/weather.ex
@@ -12,14 +12,20 @@ defmodule Weather do
       :world
 
   """
-  def temperatures_of(cities) do
+  def temperatures_of(cities, temperature_option) do
     IO.puts("Hi")
     coordinator_pid = 
       spawn(Weather.Coordinator, :loop, [[], Enum.count(cities)])
 
     cities |> Enum.each(fn city ->
       worker_pid = spawn(Weather.Worker, :loop, [])
-      send(worker_pid, {coordinator_pid, city})
+      send(worker_pid, {coordinator_pid, city, temperature_option})
     end)
   end
+
+  
+	defp url_for(location) do
+		location = URI.encode(location)
+		"http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey()}"
+	end
 end
--- lib/worker.ex
+++ lib/worker.ex
@@ -1,10 +1,12 @@
 defmodule Weather.Worker do
 	
-	def temperature_of(location) do
-		result = url_for(location) |> HTTPoison.get |> parse_response
+	def temperature_of(location, temperature_option) do
+		result = url_for(location) 
+			|> HTTPoison.get 
+			|> parse_response(temperature_option)
 		case result do
 			{:ok, temp} ->
-				"#{location}: #{temp}*C"
+				"#{location}: #{temp}*#{String.capitalize(temperature_option)}"
 			:error ->
 				"#{location} not found"
 		end
@@ -12,11 +14,11 @@ defmodule Weather.Worker do
 
 	def loop() do
 		receive do
-			{sender_pid, location} ->
-				send(sender_pid, {:ok, temperature_of(location)})
+			{sender_pid, location, temperature_option} ->
+				send(sender_pid, {:ok, temperature_of(location, temperature_option)})
 			_ ->
-				IO.puts "don't know what todo here"
-		end
+				IO.puts("don't know what todo here")
+			end
 		loop()
 	end
 
@@ -25,17 +27,19 @@ defmodule Weather.Worker do
 		"http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey()}"
 	end
 
-	defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
-		body |> JSON.decode! |> computer_temperature
+	defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}, option) do
+		body 
+		|> JSON.decode! 
+		|> computer_temperature(option)
 	end
 
-	defp parse_response(_) do
+	defp parse_response(_, _) do
 		:error
 	end
 
-	defp computer_temperature(json) do
+	defp computer_temperature(json, option) do
 		try do
-			temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+			temp = convert(json["main"]["temp"], option) |> Float.round(2)
 			{:ok, temp}
 		rescue
 			_ -> :error
@@ -44,6 +48,17 @@ defmodule Weather.Worker do
 
 	defp apikey() do
 		"8536c81ebcb074abbe45452b799dec42"
+	end
+
+	defp convert(temperature, option) do
+		cond do 
+			option in ["f", "F"] -> 
+				temperature * (9/5) - 459.67
+			option in ["c", "C"] ->
+				temperature - 273.15
+			true ->
+				temperature
+		end
 	end
 
 end
